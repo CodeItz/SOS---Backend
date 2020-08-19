e1d81fd2244fc2af189bcab8ec6e452471535394
Chrigor Eduardo <chrigor.edu3@gmail.com>
2020-08-18 21:52:11 -0300

    Corrigindo bug de importação

--- src/utils/calculateId.js
+++ src/utils/calculateId.js
@@ -1,6 +1,6 @@
 const Usuarios = require("../models/Usuario");
 const Delegacia = require("../models/Delegacia");
-const token = crypto.randomBytes(4).toString("hex");
+const crypto = require("crypto");
 
 module.exports = async function () {
   const id = crypto.randomBytes(6).toString("hex");
