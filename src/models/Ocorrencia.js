86beadb63f13eca2e2102274e0959feb89757dcc
Chrigor Eduardo <chrigor.edu3@gmail.com>
2020-08-22 21:50:48 -0300

    Adicionando last time update

--- src/models/Ocorrencia.js
+++ src/models/Ocorrencia.js
@@ -1,43 +1,48 @@
 const mongoose = require("mongoose");
 const PointSchema = require("../utils/PointSchema");
 
-const Ocorrencia = mongoose.Schema({
-  id: Number,
-  id_user: Number,
-  id_delegacia: Number,
-  avaliation: {
-    type: Number,
-    default: 0
+const Ocorrencia = mongoose.Schema(
+  {
+    id: Number,
+    id_user: Number,
+    id_delegacia: Number,
+    avaliation: {
+      type: Number,
+      default: 0,
+    },
+    comment: {
+      type: String,
+      default: "Não enviado",
+    },
+    isArmed: {
+      type: String,
+      default: "Não preenchido",
+    },
+    howManyCriminals: {
+      type: String,
+      default: "Não preenchido",
+    },
+    description: {
+      type: String,
+      default: "Não preenchido",
+    },
+    tipo: {
+      type: String,
+      default: "Não preenchido",
+    },
+    location: {
+      type: PointSchema,
+      index: "2dsphere",
+    },
+    status: {
+      type: String,
+    },
+    dateTimeStart: Date,
+    dateTimeLastUpdate: Date,
   },
-  comment: {
-    type: String,
-    default: "Não enviado"
-  },
-  isArmed: {
-    type: String,
-    default: "Não preenchido",
-  },
-  howManyCriminals: {
-    type: String,
-    default: "Não preenchido",
-  },
-  description: {
-    type: String,
-    default: "Não preenchido",
-  },
-  tipo: {
-    type: String,
-    default: "Não preenchido",
-  },
-  location: {
-    type: PointSchema,
-    index: "2dsphere",
-  },
-  status: {
-    type: String,
-  },
-  dateTimeStart: Date,
-  dateTimeLastUpdate: Date,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Ocorrencia", Ocorrencia);
